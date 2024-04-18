b27516135fcdc3bf5b6467c6cc5d6855386e9392
Nemanja Budimirovic <96budi@gmail.com>
2024-04-18 11:22:47 +0200

    Uspesno kreiran toogle navbar, ali postoji bag kad se menjaju velicine ekrana

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 import { AccountService } from 'src/app/account/account.service';
 
@@ -10,8 +11,21 @@ import { AccountService } from 'src/app/account/account.service';
 export class HeaderComponent {
 
   accountService: AccountService = inject(AccountService);
+  isOpen: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isOpen = false;
+      }
+    });
+  }
 
   onLogout(): void {
     this.accountService.logout();
+  }
+
+  onToggleNavBar(): void {
+    this.isOpen = !this.isOpen;
   }
 }
