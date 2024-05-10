cad34dbe6acaaa9dc66d5c0295fae6d6aec9e874
Nemanja Budimirovic <96budi@gmail.com>
2024-05-10 14:25:50 +0200

    Mala izmena na show more komponenti

--- src/app/shared/components/show-more/show-more.component.ts
+++ src/app/shared/components/show-more/show-more.component.ts
@@ -7,10 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class ShowMoreComponent {
 
-  isShowen: boolean = false;
-
   @Input() showText: string = '';
   @Input() showTextLength: number = 0;
+
+  isShowen: boolean = false;
   
   onChangeShow(): void {
     this.isShowen = !this.isShowen;
