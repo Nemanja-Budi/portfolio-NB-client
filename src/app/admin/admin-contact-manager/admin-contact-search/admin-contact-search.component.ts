f01a348c11824818449682681a9a72995aa240d6
Nemanja Budimirovic <96budi@gmail.com>
2024-05-10 09:30:46 +0200

    Dodata funkcionalnost sa ass i desc

--- src/app/admin/admin-contact-manager/admin-contact-search/admin-contact-search.component.ts
+++ src/app/admin/admin-contact-manager/admin-contact-search/admin-contact-search.component.ts
@@ -11,15 +11,22 @@ export class AdminContactSearchComponent {
 
   adminService: AdminService = inject(AdminService);
   
+  isAscending: boolean = this.adminService.contactQuearyParamsSubject.value.isAscending;
   itemsPerPage: number[] = [5,10,15];
 
-  onGetSearchContactInput(searchContactInput: string): void {
+  onChangeSortDirection(): void {
+    this.isAscending = !this.isAscending;
+    this.adminService.contactQuearyParamsSubject.next({
+      ...this.adminService.contactQuearyParamsSubject.value,
+      isAscending: this.isAscending
+    });
+  }
 
+  onGetSearchContactInput(searchContactInput: string): void {
     this.adminService.isSearchContactChange.next(true);
     if(searchContactInput == '') {
       this.adminService.isSearchContactChange.next(false);
     }
-
     this.adminService.contactQuearyParamsSubject.next({
       ...this.adminService.contactQuearyParamsSubject.value,
       filterQuery: searchContactInput,
